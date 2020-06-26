ea265566041a2e22371e4ac4698b32278c7815a7
Olivia Chandrasekhar <ochandra@live.unc.edu>
2020-06-26 14:53:10 -0400

    cleaning up messy test commit

--- numpy-experiments.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.linalg as la
-"Finite Difference Approximations for solving the time-independent Schrodinger"
-
-"Build the mesh"
-dx=xf/(n+1)
-
-print(dx)
